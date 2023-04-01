390b3e324672d94872e984a64a6fd29b6592e86f
Anna Gallo <61474460+galloanna@users.noreply.github.com>
2023-04-01 04:00:26 -0400

    Dev/social post cards (#1)
    * Basic UI sending data to TS file
    
    * Fix capitalization
    
    * Inserting variants + rewriting text

--- code.ts
+++ code.ts
@@ -1,33 +1,78 @@
-// This plugin will open a window to prompt the user to enter a number, and
-// it will then create that many rectangles on the screen.
-
-// This file holds the main code for the plugins. It has access to the *document*.
-// You can access browser APIs in the <script> tag inside "ui.html" which has a
-// full browser environment (see documentation).
-
-// This shows the HTML page in "ui.html".
 figma.showUI(__html__);
 
-// Calls to "parent.postMessage" from within the HTML page will trigger this
-// callback. The callback will be passed the "pluginMessage" property of the
-// posted message.
-figma.ui.onmessage = msg => {
-  // One way of distinguishing between different types of messages sent from
-  // your HTML page is to use an object with a "type" property like this.
-  if (msg.type === 'create-rectangles') {
-    const nodes: SceneNode[] = [];
-    for (let i = 0; i < msg.count; i++) {
-      const rect = figma.createRectangle();
-      rect.x = i * 150;
-      rect.fills = [{type: 'SOLID', color: {r: 1, g: 0.5, b: 0}}];
-      figma.currentPage.appendChild(rect);
-      nodes.push(rect);
-    }
-    figma.currentPage.selection = nodes;
-    figma.viewport.scrollAndZoomIntoView(nodes);
-  }
+figma.ui.resize(500, 500);
 
-  // Make sure to close the plugin when you're done. Otherwise the plugin will
-  // keep running, which shows the cancel button at the bottom of the screen.
-  figma.closePlugin();
+figma.ui.onmessage = async (pluginMessage) => {
+  await figma.loadFontAsync({ family: "Rubik", style: "Regular" });
+
+  const nodes: SceneNode[] = [];
+
+  if (pluginMessage.type === "generate-post") {
+    const postComponentSet = figma.root.findOne(
+      (node) => node.type == "COMPONENT_SET" && node.name == "post"
+    ) as ComponentSetNode;
+
+    let selectedVariant;
+
+    if (pluginMessage.darkModeState === true) {
+      switch (pluginMessage.imageVariant) {
+        case "image":
+          selectedVariant = postComponentSet.findOne(
+            (node) =>
+              node.type == "COMPONENT" &&
+              node.name == "Image=single, Dark mode=true"
+          ) as ComponentNode;
+          break;
+        default:
+          selectedVariant = postComponentSet.findOne(
+            (node) =>
+              node.type == "COMPONENT" &&
+              node.name == "Image=none, Dark mode=true"
+          ) as ComponentNode;
+          break;
+      }
+    } else {
+      switch (pluginMessage.imageVariant) {
+        case "image":
+          selectedVariant = postComponentSet.findOne(
+            (node) =>
+              node.type == "COMPONENT" &&
+              node.name == "Image=single, Dark mode=false"
+          ) as ComponentNode;
+          break;
+        default:
+          selectedVariant = postComponentSet.defaultVariant as ComponentNode;
+          break;
+      }
+    }
+
+    const newPost = selectedVariant.createInstance();
+
+    const templateName = newPost.findOne(
+      (node) => node.type == "TEXT" && node.name == "displayName"
+    ) as TextNode;
+    const templateUsername = newPost.findOne(
+      (node) => node.type == "TEXT" && node.name == "@username"
+    ) as TextNode;
+    const templateContent = newPost.findOne(
+      (node) => node.type == "TEXT" && node.name == "description"
+    ) as TextNode;
+    const numLikes = newPost.findOne(
+      (node) => node.name === "likesLabel" && node.type === "TEXT"
+    ) as TextNode;
+    const numComments = newPost.findOne(
+      (node) => node.name === "commentsLabel" && node.type === "TEXT"
+    ) as TextNode;
+
+    templateName.characters = pluginMessage.name;
+    templateUsername.characters = pluginMessage.username;
+    templateContent.characters = pluginMessage.content;
+    numLikes.characters = (Math.floor(Math.random() * 1000) + 1).toString();
+    numComments.characters = (Math.floor(Math.random() * 1000) + 1).toString();
+
+    nodes.push(newPost);
+    figma.viewport.scrollAndZoomIntoView(nodes);
+  } else if (pluginMessage.type === "close") {
+    figma.closePlugin();
+  }
 };
